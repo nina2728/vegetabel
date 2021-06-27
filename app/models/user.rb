f98f7e2b9b1eb735e9f6606a0c132ff2037b9d53
nina2728 <80686366+nina2728@users.noreply.github.com>
2021-06-27 23:47:47 +0900

    active_hashで都道府県入力

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to :prefecture
+
+  validates :prefecture_id, numericality: { other_than: 1, less_than_or_equal_to: 48 }
 end
