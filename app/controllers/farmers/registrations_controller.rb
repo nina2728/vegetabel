248ecfd8c86d6e234573f295beac33fa7e4158a4
nina2728 <80686366+nina2728@users.noreply.github.com>
2021-06-28 20:13:54 +0900

    ストロングパラメーター記述

--- app/controllers/farmers/registrations_controller.rb
+++ app/controllers/farmers/registrations_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Farmers::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
@@ -41,9 +41,9 @@ class Farmers::RegistrationsController < Devise::RegistrationsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :representative, :birthday, :postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number, :career, :comment, :point])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
