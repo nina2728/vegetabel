d29511effb9b4dbe2a1c695bb36a16d5dca19f99
nina2728 <80686366+nina2728@users.noreply.github.com>
2021-06-28 21:25:13 +0900

    バリデーション設定、修正

--- app/controllers/farmers/registrations_controller.rb
+++ app/controllers/farmers/registrations_controller.rb
@@ -42,7 +42,7 @@ class Farmers::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :representative, :birthday, :postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number, :career, :comment, :point])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :representative, :representative_kana, :birthday, :postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number, :career, :comment, :point])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
