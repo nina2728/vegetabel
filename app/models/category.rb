e2a0b5732c9496de4f0ec4b1cd99dc4f1518fb72
nina2728 <80686366+nina2728@users.noreply.github.com>
2021-07-05 20:37:02 +0900

    category_id作成

---
+++ app/models/category.rb
@@ -0,0 +1,13 @@
+class Category < ActiveHash::Base
+  self.date = [
+    { id: 1, name: '---' }
+    { id: 2, name: '果菜類' }
+    { id: 3, name: '葉茎類' }
+    { id: 4, name: '根菜類' }
+    { id: 5, name: '菌茸類' }
+    { id: 6, name: 'その他' }
+  ]
+
+  include ActiveHash::Associations
+  has_many :items
+end
