bcc5b5611ce1a68690a936a63012ea3662c4d0b5
nina2728 <80686366+nina2728@users.noreply.github.com>
2021-07-05 21:24:32 +0900

    エラーの修正

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,10 +1,10 @@
 class Category < ActiveHash::Base
   self.date = [
-    { id: 1, name: '---' }
-    { id: 2, name: '果菜類' }
-    { id: 3, name: '葉茎類' }
-    { id: 4, name: '根菜類' }
-    { id: 5, name: '菌茸類' }
+    { id: 1, name: '---' },
+    { id: 2, name: '果菜類' },
+    { id: 3, name: '葉茎類' },
+    { id: 4, name: '根菜類' },
+    { id: 5, name: '菌茸類' },
     { id: 6, name: 'その他' }
   ]
 
