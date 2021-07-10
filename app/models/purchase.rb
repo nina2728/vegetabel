2d38c22a01a6c378b480291efaa493454df6fbe9
nina2728 <80686366+nina2728@users.noreply.github.com>
2021-07-10 12:45:43 +0900

    バリデーション記述

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,3 +1,14 @@
 class Purchase < ApplicationRecord
   belongs_to :purchase_item
+
+  with_options presence: true do
+    validates :postal_code
+    validates :municipality
+    validates :address
+  end
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to :prefecture
+    
+  validates :prefecture_id, numericality: { other_than: 1, less_than_or_equal_to: 48 }
 end
