44553ac46b4a4d70ada3642a7fe1e3f023a8c8bd
nina2728 <80686366+nina2728@users.noreply.github.com>
2021-07-10 11:42:56 +0900

    purchaseカラム記述

--- db/migrate/20210710015554_create_purchases.rb
+++ db/migrate/20210710015554_create_purchases.rb
@@ -1,6 +1,12 @@
 class CreatePurchases < ActiveRecord::Migration[6.0]
   def change
     create_table :purchases do |t|
+      t.string     :postal_code,   null: false
+      t.integer    :prefecture_id, null: false
+      t.string     :municipality,  null: false
+      t.string     :address,       null: false
+      t.string     :building_name
+      t.references :purchase_item, null: false, foreign_key: true
 
       t.timestamps
     end
