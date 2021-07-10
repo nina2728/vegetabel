ff3e810270cd1ef31a009779c8ed201b9ef242bc
nina2728 <80686366+nina2728@users.noreply.github.com>
2021-07-10 09:51:45 +0900

    出品者以外が編集出来ない処理

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show, :edit, :update]
   before_action :authenticate_farmer!, except: [:index, :show]
+  before_action :item_info, only: [:edit, :show, :update]
+  before_action :item_authenticate, only: [:edit, :update]
 
   def index
     @items = Item.includes(:farmer).order('created_at DESC')
@@ -20,15 +22,12 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(params[:id])
     else
@@ -40,7 +39,18 @@ class ItemsController < ApplicationController
   end
 
   private
+
   def item_params
     params.require(:item).permit(:image, :name, :description, :contents, :category_id, :prefecture_id, :delivery_id, :shipping_id, :price).merge(farmer_id: current_farmer.id)
+  end
+
+  def item_info
+    @item = Item.find(params[:id])
+  end
+
+  def item_authenticate
+    unless @item.farmer == current_farmer
+      redirect_to root_path
+    end
   end
 end
