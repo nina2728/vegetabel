8ef62d008158aa56cdc1cabfeef6c2b91c3db186
nina2728 <80686366+nina2728@users.noreply.github.com>
2021-07-12 14:00:27 +0900

    データをテーブルに保存する処理

--- app/models/order.rb
+++ app/models/order.rb
@@ -17,4 +17,8 @@ class Order
     belongs_to :prefecture
 
   validates :prefecture_id, numericality: { other_than: 1, less_than_or_equal_to: 48 }
+
+  def save
+    purchase_item = PurchaseItem.create(user_id: user_id, item_id: item_id)
+    Purchase.create(postal_code: postal_code, prefecture_id: prefecture_id, municipality: municipality, address: address, building_name: building_name, purchase_id: purchase.id)
 end
