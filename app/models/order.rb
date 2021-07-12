16db01fa074bb922af928ee9d3f12bdc93e5b36c
nina2728 <80686366+nina2728@users.noreply.github.com>
2021-07-12 13:51:17 +0900

    バリデーション修正

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,13 +2,19 @@ class Order
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :municipality, :address, :building_name, :token
 
+  POSTAL_REGEX   = /\A\d{3}[-]\d{4}$|^\d{3}[-]\d{2}$|^\d{3}\z/
+  
   with_options presence: true do
     validates :user_id
     validates :item_id
-    validates :postal_code
-    validates :prefecture_id
+    validates :postal_code, format: { with: POSTAL_REGEX }
     validates :municipality
     validates :address
     validates :token
   end
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to :prefecture
+
+  validates :prefecture_id, numericality: { other_than: 1, less_than_or_equal_to: 48 }
 end
--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,16 +1,3 @@
 class Purchase < ApplicationRecord
   belongs_to :purchase_item
-
-  POSTAL_REGEX   = /\A\d{3}[-]\d{4}$|^\d{3}[-]\d{2}$|^\d{3}\z/
-
-  with_options presence: true do
-    validates :postal_code,  format: { with: POSTAL_REGEX }
-    validates :municipality
-    validates :address
-  end
-
-  extend ActiveHash::Associations::ActiveRecordExtensions
-    belongs_to :prefecture
-    
-  validates :prefecture_id, numericality: { other_than: 1, less_than_or_equal_to: 48 }
 end
