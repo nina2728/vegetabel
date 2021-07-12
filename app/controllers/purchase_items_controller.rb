bb8ed780c43616afe179569e70511f444557be92
nina2728 <80686366+nina2728@users.noreply.github.com>
2021-07-12 13:35:01 +0900

    createアクション定義

--- app/controllers/purchase_items_controller.rb
+++ app/controllers/purchase_items_controller.rb
@@ -4,4 +4,18 @@ class PurchaseItemsController < ApplicationController
     @order = Order.new
   end
   
+  def create
+    @order = Order.create(order_params)
+    if @order.valid?
+      @order.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(:item_id, :postal_code, :prefecture_id, :municipality, :address, :building_name).merge(:user_id current_user.id)
+  end
 end
