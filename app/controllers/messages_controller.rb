4c20c89fdf4324ca2ab06ffa8ba6dd549230390a
nina2728 <80686366+nina2728@users.noreply.github.com>
2021-07-26 21:47:29 +0900

    コントローラー編集

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,9 @@
 class MessagesController < ApplicationController
   def new
+    @messages = Message.all
+    @Message = Message.new
   end
+
+  def create
+    @message = Message.new(text: params[:message][:text])
 end
